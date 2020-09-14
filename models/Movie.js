bbb41cdc425059497d777e67ee74e4cf4c8d7a42
bedii97 <bedii97@gmail.com>
2020-09-14 11:13:52 +0300

    Improves

--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,18 @@ const MovieSchema = new Schema({
     director_id: Schema.Types.ObjectId,
     title: {
         type: String,
-        required: true
+        required: [true, '`{PATH}` alanı zorunludur'],
+        maxlength: [50, '`{PATH}` alanı (`{VALUE}`), (`{MAXLENGTH}`) karakterden küçük olmalıdır'],
+        minlength: [2, '`{PATH}` alanı (`{VALUE}`), (`{MINLENGTH}`) karakterden büyük olmalıdır'],
     },
     category: String,
     country: String,
     year: Number,
-    imdb_score: Number,
+    imdb_score: {
+      type: Number,
+      max: 10,
+      min: 0  
+    },
     date: {
         type: Date,
         default: Date.now
--- routes/director.js
+++ routes/director.js
@@ -5,7 +5,7 @@ var router = express.Router();
 //Models
 const Director = require('../models/Director');
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   const promise = Director.aggregate([
     {
       $lookup: {
@@ -23,16 +23,16 @@ router.get('/', function(req, res, next) {
     },
     {
       $group: {
-        _id:{
+        _id: {
           _id: '$_id',
           name: '$name',
           surname: '$surname',
           bio: '$bio'
         },
-        movie_count:{
+        movie_count: {
           $sum: 1
         },
-        movies:{
+        movies: {
           $push: '$movies'
         }
       }
@@ -49,18 +49,18 @@ router.get('/', function(req, res, next) {
     }
   ]);
 
-  promise.then((data)=>{
+  promise.then((data) => {
     res.json(data);
-  }).catch((err)=>{
+  }).catch((err) => {
     res.json(err);
   });
 });
 
-router.get('/:director_id', function(req, res, next) {
+router.get('/:director_id', function (req, res, next) {
   const promise = Director.aggregate([
     {
       $match: {
-        '_id':mongoose.Types.ObjectId(req.params.director_id)
+        '_id': mongoose.Types.ObjectId(req.params.director_id)
       }
     },
     {
@@ -79,16 +79,16 @@ router.get('/:director_id', function(req, res, next) {
     },
     {
       $group: {
-        _id:{
+        _id: {
           _id: '$_id',
           name: '$name',
           surname: '$surname',
           bio: '$bio'
         },
-        movie_count:{
+        movie_count: {
           $sum: 1
         },
-        movies:{
+        movies: {
           $push: '$movies'
         }
       }
@@ -105,20 +105,47 @@ router.get('/:director_id', function(req, res, next) {
     }
   ]);
 
-  promise.then((data)=>{
+  promise.then((data) => {
     res.json(data);
-  }).catch((err)=>{
+  }).catch((err) => {
     res.json(err);
   });
 });
 
-router.post('/', function(req, res, next) {
+router.put('/:director_id', (req, res, next) => {
+  const promise = Director.findByIdAndUpdate(
+    req.params.director_id,
+    req.body,
+    {
+      new: true //Yeni datayı döndürür
+    });
+  promise.then((director) => {
+    if (!director)
+      next({ message: 'The director was not found', code: 1 }); //app.js line 41
+    res.json({ status: 1, data: director });
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.delete('/:director_id', (req, res, next) => {
+  const promise = Director.findByIdAndRemove(req.params.director_id);
+  promise.then((director) => {
+    if(!director)
+      next({message: 'The director was not found', code: 1}); //app.js line 41
+    res.json({status: 1});
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.post('/', function (req, res, next) {
   const director = new Director(req.body);
   const promise = director.save();
 
-  promise.then((data)=>{
+  promise.then((data) => {
     res.json(data);
-  }).catch((err)=>{
+  }).catch((err) => {
     res.json(err);
   });
 });
